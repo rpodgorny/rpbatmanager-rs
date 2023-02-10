62fd6c24b5171fd17f11f411946e6697e2a403c7
Radek Podgorny <radek@podgorny.cz>
2023-02-10 20:19:57 +0100

    migrate to anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::error::Error;
+use anyhow::Result;
 
 const PREFIX: &str = "/sys/class/power_supply/BAT0";
 const MIN: i32 = 60;
 const MAX: i32 = 80;
 const SLEEP: u64 = 2;
 
-fn set_thresh(v: i32) -> Result<(), Box<dyn Error>> {
+fn set_thresh(v: i32) -> Result<()> {
     println!("NEW_THRESH {v}");
     std::fs::write(
         format!("{PREFIX}/charge_control_end_threshold"),
@@ -14,16 +14,17 @@ fn set_thresh(v: i32) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn my_read_to_string<P>(p: P) -> Result<String, Box<dyn Error>>
+fn my_read_to_string<P>(p: P) -> Result<String>
 where
     P: AsRef<std::path::Path>,
 {
     Ok(std::fs::read_to_string(p)?.trim().to_string())
 }
 
-fn tick() -> Result<(), Box<dyn Error>> {
+fn tick() -> Result<()> {
     let status = my_read_to_string(format!("{PREFIX}/status"))?;
-    let cur_thresh = my_read_to_string(format!("{PREFIX}/charge_control_end_threshold"))?.parse::<i32>()?;
+    let cur_thresh =
+        my_read_to_string(format!("{PREFIX}/charge_control_end_threshold"))?.parse::<i32>()?;
     let new_thresh = if cur_thresh == 100 || status == "Discharging" {
         let level = my_read_to_string(format!("{PREFIX}/capacity"))?.parse::<i32>()?;
         Some(if level < MIN { MAX } else { MIN })
@@ -39,7 +40,7 @@ fn tick() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     println!("starting rpbatmanager v{}", env!("CARGO_PKG_VERSION"));
     println!("PREFIX {PREFIX}");
     if std::env::args().any(|x| x == "full") {
